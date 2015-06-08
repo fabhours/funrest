70b73d499e1f15b5c9dcc1536ed9913827842bb1
Muhammad Sigit Dude <dudelectric@gmail.com>
2015-06-08 14:31:23 +0700

    add price1 to game

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608035324) do
+ActiveRecord::Schema.define(version: 20150608072925) do
 
   create_table "cards", force: true do |t|
     t.string   "number"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150608035324) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "price1"
   end
 
   create_table "kartus", force: true do |t|
